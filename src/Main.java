1acd67d0133a7ed6d2b4ef7bb26e8da1711891e4
jordonjones <jordon01jones@gmail.com>
2026-01-02 14:02:16 -0600

    function14

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+// Print All Multiples of Three (Loop)
+// Write a function that prints and returns
+// an array of all multiples of 3 less than a given int.
+
+
+import java.util.List ;
+import java.util.ArrayList ;
 
 public class Main {
+    public static List<Integer> multOfThree(int n){
+        ArrayList<Integer> multiples = new ArrayList<>();
+        for( int i = 1 ; i < n ; i++ ){
+            if(i % 3 == 0){
+                multiples.add(i);
+            }
+        }
+        return multiples ;
+
+    }
+    public static void main(String[] args){
+        System.out.println(multOfThree(100)) ;
+    }
+
 }
